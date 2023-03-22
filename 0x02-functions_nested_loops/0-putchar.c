9fddadf82966046e7a5823d30f233d29c85f31e2
ousamahaddadi <ousama.haddadi@gmail.com>
2023-03-22 07:18:42 -0700

    add 0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include <unistd.h>
+#include "main.h"
+/**
+ * main: the first entry
+ * Return: always 0
+ */
+int main(void)
+{
+	char str[] = "_putchar";
+	int i;
+
+	i = 0;
+
+	while(str[i])
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+	return 0;
+}
+

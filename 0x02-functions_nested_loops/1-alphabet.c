a32d18865cbf903c8f4677b4b18d6f7c6baea519
ousamahaddadi <ousama.haddadi@gmail.com>
2023-03-22 11:06:14 -0700

    add 1-alphabet.c function

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_alphabet- prints alphabets
+ * return: void
+ */
+void print_alphabet(void)
+{
+	int i = 97;
+
+	while (i < 123)
+		_putchar(i++);
+	_putchar('\n');
+}
+
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,2 +1,8 @@
+#ifndef MAIN_H
+#define MAIN_H
+
 int _putchar(char c);
 int main(void);
+void print_alphabet(void);
+
+#endif
